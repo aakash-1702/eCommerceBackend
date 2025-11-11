2538c0ec7686ca32f004502d7b9535ef7494a47f
HaseethVardhan <haseethvardhan01@gmail.com>
2025-11-11 19:09:52 +0530

    added db connection

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,1 +1,21 @@
-// temp data
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './.env'
+});
+
+const connectDB = async () => {
+    try {
+        const sql = neon(process.env.DATABASE_URL);
+        // const result = await sql`SELECT * FROM employees`;
+        // console.log(`Employees retrieved successfully:`, result);
+        
+        return sql;
+    } catch (error) {
+        console.log(`Query FAILED: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
